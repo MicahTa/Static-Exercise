8b607393976c3715a02308d0ecd96dc82971c0d5
Laptop <micahcorbintanner@gmail.com>
2025-05-12 08:48:46 -0600

    Did the assignmennt

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"32 degrees celsius is {TempConverter.CelsiusToFahrenheit(32)} degrees fahrenheit.");
+            Console.WriteLine($"89.6 degrees fahrenheit is {TempConverter.FahrenheitToCelsius(89.6)} degrees celsius.");
         }
     }
 }
